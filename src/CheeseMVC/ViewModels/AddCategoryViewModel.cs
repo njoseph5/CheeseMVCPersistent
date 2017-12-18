283a3c704545d5846775b6bc5ef0b59fbc09a51a
Neethu Joseph <vabsaxena@gmail.com>
2017-12-18 13:57:25 -0600

    completed part 1

---
+++ src/CheeseMVC/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCategoryViewModel
+    {
+		[Required(ErrorMessage = "You must give your cheese a description")]
+		[Display(Name = "Category Name")]
+	
+		public string Name { get; set; }
+
+		
+	}
+}
